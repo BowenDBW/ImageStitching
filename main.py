7b88535a76389f7042058212c60a6fa1776f17c9
bowen <2631702650@qq.com>
2022-12-21 22:05:01 +0800

    inti2

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 import cv2
-import numpy
+import numpy as np
+import matplotlib.pyplot as plt
+import warnings
+import glob
 
-# 暴力匹配
-img1 = cv2.imread('imgs/1-1.jpg')
-img2 = cv2.imread("imgs/1-2.jpg")
+cv2.ocl.setUseOpenCL(False)
+warnings.filterwarnings('ignore')
+feature_extraction_algo = "sift"
+feature_to_match = 'bf'
 
+# 读入图片
+image_path = glob.glob('imgs/src_2/*.jpg')
+images = ()
+for image in image_path:
+    img = cv2.imread(image)
+    img = cv2.resize(img, (1280, 720))
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    images.append(img)
 
-def show(name, img):
-    cv2.imshow(name, img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+# 展示图片
+fig, images = plt.subplots(nrows=1, ncols=images.count(), constrained_layout= False, figsize=(16, 9))
+for image in
 
-
-show("img1", img1)
-
+    https: // www.youtube.com / watch?v = uMABRY8QPe0
